277e0c38c5c31a7f3c85ec6e84dd31a33145d805
compiledge <eduardo.iftm@gmail.com>
2023-10-24 12:34:59 -0300

    feat: Install noice (cmd plugin) in neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -514,6 +514,22 @@ require("lazy").setup({
 	-- [Utilitários]{{{
 	{"sitiom/nvim-numbertoggle"},
 
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		}
+	},
+
    {
         'rcarriga/nvim-notify',
         config = function()
@@ -523,6 +539,7 @@ require("lazy").setup({
             vim.notify = require("notify")
         end
     },
+
 	 {
 		 'norcalli/nvim-colorizer.lua',
 		 keys = {
