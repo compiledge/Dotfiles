148de91b5324fb86bda5603a0ac657a13bdf451e
compiledge <eduardo.iftm@gmail.com>
2023-11-13 19:38:58 -0300

    feat: Lualine integration with showcmd and counts

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -381,39 +381,28 @@ require("lazy").setup({
 	{ 'rose-pine/neovim', name = 'rose-pine' },
 
 	{
-		 'nvim-lualine/lualine.nvim',
-		 dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-		 config = function()
-			 require("lualine").setup({
-				 sections = {
-					 lualine_x = {
-						 {
-							 require("noice").api.status.message.get_hl,
-							 cond = require("noice").api.status.message.has,
-						 },
-						 {
-							 require("noice").api.status.command.get,
-							 cond = require("noice").api.status.command.has,
-							 color = { fg = "#ff9e64" },
-						 },
-						 {
-							 require("noice").api.status.mode.get,
-							 cond = require("noice").api.status.mode.has,
-							 color = { fg = "#ff9e64" },
-						 },
-						 {
-							 require("noice").api.status.search.get,
-							 cond = require("noice").api.status.search.has,
-							 color = { fg = "#ff9e64" },
-						 },
-					 },
-				 },
-			 })
-			 options = {
-				 theme = 'tokyonight'
-			 }
-		 end
-	 },
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
+		config = function()
+
+			vim.opt.showcmdloc='statusline'
+			require("lualine").setup({
+				sections = {
+
+					lualine_a = {'mode'},
+					lualine_b = {'branch', 'diff', 'diagnostics'},
+					lualine_c = {'filename'},
+					lualine_x = {'%S', 'searchcount', 'selectioncount', 'encoding', 'fileformat', 'filetype'},
+					lualine_y = {'progress'},
+					lualine_z = {'location'}
+
+				},
+			})
+			options = {
+				theme = 'tokyonight'
+			}
+		end
+	},
 
 	 {
 		 'glepnir/dashboard-nvim',
