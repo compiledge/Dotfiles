aac87b57e9e50fa5a4223fe7c01ce2652ce1a54a
compiledge <eduardo.iftm@gmail.com>
2023-11-21 22:28:13 -0300

    ci: Upgrade the neovim config
    Some upgrades:
    - Remove the visual.lua file
    - Add a header to lazy.lua
    - Add an autocmds.lua file
    - Set custom options to options.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,3 +1,8 @@
+--
+-- ░█░░░█▀█░▀▀█░█░█░░░░█░░░█░█░█▀█
+-- ░█░░░█▀█░▄▀░░░█░░░░░█░░░█░█░█▀█
+-- ░▀▀▀░▀░▀░▀▀▀░░▀░░▀░░▀▀▀░▀▀▀░▀░▀
+--
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,62 +5,14 @@
 --
 
 -- Invisible chars style
-vim.o.listchars = "tab:▷-,eol:↴,trail:~,extends:>,precedes:<,space:⋅"
 vim.o.list = false -- Show some invisible characters (tabs...
+vim.o.listchars = "tab:▷-,eol:↴,trail:~,extends:>,precedes:<,space:⋅"
 
--- --Indentação
--- vim.o.smartindent = true --Automaticamente insere um nível extra de identação
--- vim.o.smarttab = true --Automaticamente insere tab no início da linha
--- vim.o.tabstop = 3 --Mostra os tabs como 3 espaços
--- vim.o.shiftwidth = 3 --Quando identando com '>', usa 3 espaços
--- vim.o.expandtab = false --Tab pro espaço? Tab venceu a guerra :)
---
--- -- Scroll
--- vim.o.scrolloff = 8 -- Máximo de linhas até rolar a janela
--- vim.sidescrolloff = 5
---
--- --Sprits
--- vim.o.splitright = true --Jogar vsplits para a direita
--- vim.o.splitbelow = true --Jogar splits para baixo
---
--- -- Contador de linhas
--- vim.o.number = true -- Habilitando linhas absolutas e...
--- vim.o.relativenumber = true -- ... combinando linhas relativas
--- vim.o.cursorline = true -- Destacar a linha atual?
--- vim.o.cursorcolumn = false -- Destacar a coluna atual?
---
--- -- Configurações Gerais
--- vim.opt.autochdir = true --Atualizar para diretório atual do arquivo
---
--- -- Folding
--- vim.o.foldmethod = "marker" --Destacando folding com as marcas {{{}}}
---
--- -- Buscas
--- vim.o.ignorecase = true --Ignorar maiúsculas e minúsculas
--- vim.o.smartcase = true --Ignora o case até que a digitação seja mais precisa
--- vim.o.incsearch = true --Elementos destacados enquanto digitamos a busca
--- vim.o.hlsearch = true --Colorir resultados
---
--- --[Dicionário]
--- vim.opt.spelllang = { "pt_br" }
---
--- --[Linguagem dos menus]
--- -- vim.api.nvim_exec ('language en_US.utf8', true)
--- vim.cmd([[language en_US.utf8]])
---
--- --[Linhas]
--- vim.o.linebreak = true -- display lines que não quebram palavras
---
--- --[Clipboard]
--- vim.opt.clipboard = "unnamedplus"
---
--- --[Menus]
--- vim.opt.completeopt = "menu,menuone,noselect"
---
--- --[Terminal]
--- vim.cmd([[autocmd TermOpen * setlocal nonumber norelativenumber]])
---
--- --[Mardkown]
--- -- Set code highlighting to markdown code blocks
--- vim.g.markdown_fenced_languages = { "c", "python", "bash" }
--- vim.cmd([[let g:markdown_folding = 1]])
+-- Jump to the file directory
+vim.opt.autochdir = true
+
+-- Spell language
+vim.opt.spelllang = { "pt_br" }
+
+-- Menu language
+vim.cmd([[language en_US.utf8]])
